5fcdc01ad07747b05e3c904c70738ec2ea0f5a75
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-02-01 21:34:30 +0530

    Controller class for sudoku server.

---
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/controller/SudokuServerController.java
@@ -0,0 +1,12 @@
+package io.github.himanshusajwan911.sudokuserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sudoku-server")
+public class SudokuServerController {
+
+	
+	
+}
