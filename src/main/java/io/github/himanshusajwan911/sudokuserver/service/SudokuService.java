dc51b348f723ab717e8a99d97f938b4eb725784b
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-02-10 18:25:10 +0530

    Added enum Level for sudoku generation.

--- src/main/java/io/github/himanshusajwan911/sudokuserver/service/SudokuService.java
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/service/SudokuService.java
@@ -5,5 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class SudokuService {
 
+	public enum Level {
+		EASY(15), MEDIUM(25), HARD(40), VERY_HARD(60), INSANE(70);
+
+		private final int cellRemovePercent;
+
+		Level(int value) {
+			this.cellRemovePercent = value;
+		}
+
+		public int getCellRemovePercent() {
+			return cellRemovePercent;
+		}
+	}
+	
 	
 }
