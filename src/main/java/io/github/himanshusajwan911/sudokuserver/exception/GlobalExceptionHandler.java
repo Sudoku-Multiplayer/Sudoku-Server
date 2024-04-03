37878aafc557e1f9b9d4126acb1b8710d2f54d40
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-03 16:22:33 +0530

    Global Exception Handler class.

---
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package io.github.himanshusajwan911.sudokuserver.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+		return new ResponseEntity<>("Malformed request body: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(NoSuchPlayerExistsException.class)
+	public ResponseEntity<String> handleNoSuchPlayerExistsException(NoSuchPlayerExistsException ex) {
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+
+}
