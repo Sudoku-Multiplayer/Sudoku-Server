02b7683f08ed9122c7f6d16710509a653989548b
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-27 16:27:46 +0530

    Model for GameSession.
    This class will contains data for a game session such as:
    GameSessionRunner object, game thread object, List of game chat messages
    and List of board updates.

---
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/model/GameSession.java
@@ -0,0 +1,97 @@
+package io.github.himanshusajwan911.sudokuserver.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.himanshusajwan911.sudokuserver.async.GameSessionRunner;
+import io.github.himanshusajwan911.sudokuserver.exception.GameFinishedException;
+import io.github.himanshusajwan911.sudokuserver.model.SudokuGame.SudokuGameStatus;
+import io.github.himanshusajwan911.sudokuserver.service.NotificationService;
+
+public class GameSession {
+
+	private SudokuGame game;
+	private GameSessionRunner gameSessionRunner;
+	private Thread gameThread;
+	private NotificationService notificationService;
+
+	private List<GameChatMessage> gameChatMessages;
+	private List<BoardUpdate> boardUpdates;
+
+	public GameSession(SudokuGame game, NotificationService notificationService) {
+		this.game = game;
+		this.notificationService = notificationService;
+		gameSessionRunner = new GameSessionRunner(game, notificationService);
+		gameThread = new Thread(gameSessionRunner);
+		this.gameChatMessages = new ArrayList<>();
+		this.boardUpdates = new ArrayList<>();
+	}
+
+	public SudokuGame getGame() {
+		return game;
+	}
+
+	public GameSessionRunner getGameSessionRunner() {
+		return gameSessionRunner;
+	}
+
+	public Thread getGameThread() {
+		return gameThread;
+	}
+
+	public void startGame() {
+		if (game.getStatus() == SudokuGameStatus.FINISHED) {
+			throw new GameFinishedException("Game is already finished.");
+		} else {
+			if (game.getStatus() == SudokuGameStatus.NEW) {
+				game.setStatus(SudokuGameStatus.RUNNING);
+				gameThread.start();
+			}
+		}
+	}
+
+	public void stopGame() {
+		gameSessionRunner.stopGame();
+	}
+
+	public void pauseGame() {
+		if (game.getStatus() == SudokuGameStatus.NEW) {
+			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(),
+					"Cannot pause, Game is not started yet.");
+		} else if (game.getStatus() == SudokuGameStatus.FINISHED) {
+			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(), "Game is Finished, cannot pause.");
+		} else {
+			gameSessionRunner.pauseGame();
+		}
+	}
+
+	public void resumeGame() {
+		if (game.getStatus() == SudokuGameStatus.NEW) {
+			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(),
+					"Cannot resume, Game is not started yet.");
+		} else if (game.getStatus() == SudokuGameStatus.FINISHED) {
+			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(), "Cannot resume, Game is Finished.");
+		} else {
+			if (game.getStatus() == SudokuGameStatus.PAUSED) {
+				gameSessionRunner.resumeGame();
+			}
+		}
+	}
+
+	public void addBoardUpdate(BoardUpdate boardUpdate) {
+		boardUpdates.add(boardUpdate);
+	}
+
+	public List<BoardUpdate> getBoardUpdates() {
+		return boardUpdates;
+	}
+
+	public void addGameChatMessage(GameChatMessage gameChatMessage) {
+		gameChatMessages.add(gameChatMessage);
+	}
+
+	public List<GameChatMessage> getGameChatMessages() {
+		return gameChatMessages;
+	}
+
+}
