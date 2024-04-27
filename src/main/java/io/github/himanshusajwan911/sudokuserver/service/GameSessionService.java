29ba1c3854fdbaefa24f617d75d48ab5bfc6048a
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-27 16:56:31 +0530

    Service class for GameSession.

---
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/service/GameSessionService.java
@@ -0,0 +1,74 @@
+package io.github.himanshusajwan911.sudokuserver.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import io.github.himanshusajwan911.sudokuserver.model.BoardUpdate;
+import io.github.himanshusajwan911.sudokuserver.model.GameChatMessage;
+import io.github.himanshusajwan911.sudokuserver.model.GameSession;
+import io.github.himanshusajwan911.sudokuserver.model.SudokuGame;
+import io.github.himanshusajwan911.sudokuserver.repository.GameSessionRepository;
+
+@Service
+public class GameSessionService {
+
+	private GameSessionRepository gameSessionRepository;
+
+	private NotificationService notificationService;
+
+	public GameSessionService(GameSessionRepository gameSessionRepository, NotificationService notificationService) {
+		this.gameSessionRepository = gameSessionRepository;
+		this.notificationService = notificationService;
+	}
+
+	public GameSession createGameSession(String gameId, SudokuGame game) {
+		GameSession gameSession = new GameSession(game, notificationService);
+		gameSessionRepository.addGameSession(gameId, gameSession);
+
+		return gameSession;
+	}
+
+	public void startGame(String gameId) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+		gameSession.startGame();
+	}
+
+	public void stopGame(String gameId) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+		gameSession.stopGame();
+	}
+
+	public void pauseGame(String gameId) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+		gameSession.pauseGame();
+	}
+
+	public void resumeGame(String gameId) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+		gameSession.resumeGame();
+	}
+
+	public void addBoardUpdate(String gameId, BoardUpdate boardUpdate) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+		gameSession.addBoardUpdate(boardUpdate);
+	}
+
+	public List<BoardUpdate> getBoardUpdates(String gameId) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+
+		return gameSession.getBoardUpdates();
+	}
+
+	public void addGameChatMessage(String gameId, GameChatMessage gameChatMessage) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+		gameSession.addGameChatMessage(gameChatMessage);
+	}
+
+	public List<GameChatMessage> getGameChatMessages(String gameId) {
+		GameSession gameSession = gameSessionRepository.getGameSession(gameId);
+
+		return gameSession.getGameChatMessages();
+	}
+
+}
