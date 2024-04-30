524ce16dbd8dfdebdebfb17496222de3fefbea3c
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-30 23:55:32 +0530

    added destinationPrefixe "/game-session"

--- src/main/java/io/github/himanshusajwan911/sudokuserver/congif/WebSocketConfig.java
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/congif/WebSocketConfig.java
@@ -1,9 +1,7 @@
 package io.github.himanshusajwan911.sudokuserver.congif;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -14,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/game-updates");
+		config.enableSimpleBroker("/game-updates", "/game-session");
 		config.setApplicationDestinationPrefixes("/game");
 	}
 
