23201bc4f7f9a4e8eb1205edb3873e59418b0e91
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-30 22:32:13 +0530

    Added new methods and REST end points.

--- src/main/java/io/github/himanshusajwan911/sudokuserver/controller/SudokuServerController.java
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/controller/SudokuServerController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.github.himanshusajwan911.sudokuserver.exception.InvalidLevelException;
 import io.github.himanshusajwan911.sudokuserver.exception.NoSolutionException;
+import io.github.himanshusajwan911.sudokuserver.model.GameBoard;
+import io.github.himanshusajwan911.sudokuserver.repository.GameSubscriptionRespository;
 import io.github.himanshusajwan911.sudokuserver.service.SudokuService;
 import io.github.himanshusajwan911.sudokuserver.service.SudokuService.Level;
 
@@ -24,6 +26,9 @@ public class SudokuServerController {
 
 	@Autowired
 	private SudokuService sudokuService;
+
+	@Autowired
+	private GameSubscriptionRespository gameSubscriptionRespository;
 
 	@CrossOrigin(origins = "*")
 	@GetMapping("/generate-board-by-level")
@@ -39,8 +44,41 @@ public class SudokuServerController {
 		}
 
 		Level levelEnum = Level.valueOf(level.toUpperCase());
+		int[][] sudokuBoard = null;
+		try {
+			sudokuBoard = sudokuService.generateSudokuBoard(boardSize, levelEnum).getBoard();
 
-		return new ResponseEntity<>(sudokuService.generateSudokuBoard(boardSize, levelEnum), HttpStatus.OK);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("Error in creating a sudoku board of size: " + boardSize);
+		}
+
+		return new ResponseEntity<>(sudokuBoard, HttpStatus.OK);
+	}
+
+	@CrossOrigin(origins = "*")
+	@GetMapping("/generate-board-solution-by-level")
+	public ResponseEntity<GameBoard> generateSudokuBoardAndSolution(
+			@RequestParam(name = "boardSize", defaultValue = "9") int boardSize,
+			@RequestParam(name = "level", defaultValue = "EASY") String level) {
+
+		if (level != null) {
+			if (!Set.of("EASY", "MEDIUM", "HARD", "VERY_HARD", "INSANE").contains(level.toUpperCase())) {
+				throw new InvalidLevelException(
+						"Invalid level provided.\nValid levels are: EASY, MEDIUM, HARD, VERY_HARD, INSANE");
+			}
+		}
+
+		Level levelEnum = Level.valueOf(level.toUpperCase());
+
+		GameBoard gameBoard = null;
+		try {
+			gameBoard = sudokuService.generateSudokuBoard(boardSize, levelEnum);
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("Error in creating a sudoku board of size: " + boardSize);
+		}
+
+		return new ResponseEntity<>(gameBoard, HttpStatus.OK);
 	}
 
 	@CrossOrigin(origins = "*")
@@ -49,9 +87,19 @@ public class SudokuServerController {
 			@RequestParam(name = "boardSize", defaultValue = "9") int boardSize,
 			@RequestParam(name = "cellRemove", defaultValue = "0") int cellRemove) {
 
+		return new ResponseEntity<>(sudokuService.generateSudokuBoard(boardSize, cellRemove).getBoard(), HttpStatus.OK);
+	}
+
+	@CrossOrigin(origins = "*")
+	@GetMapping("/generate-board-solution-by-cell-remove")
+	public ResponseEntity<GameBoard> generateSudokuBoardAndSolution(
+			@RequestParam(name = "boardSize", defaultValue = "9") int boardSize,
+			@RequestParam(name = "cellRemove", defaultValue = "0") int cellRemove) {
+
 		return new ResponseEntity<>(sudokuService.generateSudokuBoard(boardSize, cellRemove), HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "*")
 	@PostMapping("/solve-sudoku-board")
 	public ResponseEntity<int[][]> solveSudokuBoard(@RequestBody int[][] sudokuBoard) {
 
@@ -61,6 +109,14 @@ public class SudokuServerController {
 		}
 
 		return new ResponseEntity<>(sudokuBoard, HttpStatus.OK);
+	}
+
+	@CrossOrigin("*")
+	@GetMapping("/online-player-count")
+	public ResponseEntity<Integer> getOnlinePlayerCount() {
+		System.out.println("getOnlinePlayerCount()");
+		System.out.println(gameSubscriptionRespository.getOnlinePlayerCount());
+		return new ResponseEntity<Integer>(gameSubscriptionRespository.getOnlinePlayerCount(), HttpStatus.OK);
 	}
 
 }
