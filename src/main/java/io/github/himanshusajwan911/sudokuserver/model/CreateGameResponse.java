6fd6753b86443edbbe870e34f849502b2e3e8d0a
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-02 00:16:06 +0530

    Model class for sending response of CreateGameRequest.

---
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/model/CreateGameResponse.java
@@ -0,0 +1,15 @@
+package io.github.himanshusajwan911.sudokuserver.model;
+
+public class CreateGameResponse {
+
+	private String gameId;
+
+	public String getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(String gameId) {
+		this.gameId = gameId;
+	}
+
+}
