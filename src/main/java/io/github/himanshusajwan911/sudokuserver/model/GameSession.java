dd45a2f43e3c42a81999c2e5d8969c443b4179af
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-09 22:21:36 +0530

    Added Player and Voting mechanism.
    - Added LinkedHashMap to store Player with their PlayerSession.
    - Added methods to control addition and removal of players.
    - Added methods to initiate vote and cast vote.

--- src/main/java/io/github/himanshusajwan911/sudokuserver/model/GameSession.java
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/model/GameSession.java
@@ -1,22 +1,31 @@
 package io.github.himanshusajwan911.sudokuserver.model;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import io.github.himanshusajwan911.sudokuserver.async.GameSessionRunner;
 import io.github.himanshusajwan911.sudokuserver.exception.GameFinishedException;
+import io.github.himanshusajwan911.sudokuserver.exception.GameNotStartedException;
 import io.github.himanshusajwan911.sudokuserver.model.SudokuGame.SudokuGameStatus;
 import io.github.himanshusajwan911.sudokuserver.service.NotificationService;
 
 public class GameSession {
 
 	private SudokuGame game;
+
 	private GameSessionRunner gameSessionRunner;
 	private Thread gameThread;
+
 	private NotificationService notificationService;
+
+	private int gameSubmitVoteCount;
 
 	private List<GameChatMessage> gameChatMessages;
 	private List<BoardUpdate> boardUpdates;
+
+	private Map<Player, PlayerSession> playerSessionMap;
 
 	public GameSession(SudokuGame game, NotificationService notificationService) {
 		this.game = game;
@@ -25,6 +34,7 @@ public class GameSession {
 		gameThread = new Thread(gameSessionRunner);
 		this.gameChatMessages = new ArrayList<>();
 		this.boardUpdates = new ArrayList<>();
+		this.playerSessionMap = new LinkedHashMap<>();
 	}
 
 	public SudokuGame getGame() {
@@ -70,13 +80,40 @@ public class GameSession {
 		if (game.getStatus() == SudokuGameStatus.NEW) {
 			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(),
 					"Cannot resume, Game is not started yet.");
-		} else if (game.getStatus() == SudokuGameStatus.FINISHED) {
+		}
+
+		else if (game.getStatus() == SudokuGameStatus.FINISHED) {
 			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(), "Cannot resume, Game is Finished.");
-		} else {
+		}
+
+		else {
 			if (game.getStatus() == SudokuGameStatus.PAUSED) {
 				gameSessionRunner.resumeGame();
 			}
 		}
+	}
+
+	public void addPlayer(Player player) {
+		PlayerSession playerSession = new PlayerSession();
+		playerSession.setPlayer(player);
+		playerSession.setVoteStatus(VoteStatus.WAITING);
+
+		playerSessionMap.put(player, playerSession);
+	}
+
+	public boolean removePlayer(Player player) {
+		PlayerSession playerSession = playerSessionMap.remove(player);
+		Player removedPlayer = playerSession.getPlayer();
+
+		return player.equals(removedPlayer);
+	}
+
+	public List<Player> getJoinedPlayers() {
+		return new ArrayList<Player>(playerSessionMap.keySet());
+	}
+
+	public int getPlayerCount() {
+		return playerSessionMap.size();
 	}
 
 	public void addBoardUpdate(BoardUpdate boardUpdate) {
@@ -93,6 +130,55 @@ public class GameSession {
 
 	public List<GameChatMessage> getGameChatMessages() {
 		return gameChatMessages;
+	}
+
+	public void initiateGameSubmitVoting(Player player) {
+		if (game.getStatus() == SudokuGameStatus.NEW) {
+			throw new GameNotStartedException("cannot initiate Game Submit Voting, game is not Started yet.");
+		}
+
+		if (game.getStatus() == SudokuGameStatus.FINISHED) {
+			throw new GameFinishedException("cannot initiate Game Submit Voting, game is already Finsished.");
+		}
+
+		for (PlayerSession playerSession : playerSessionMap.values()) {
+			playerSession.resetVoteStatus();
+		}
+
+		gameSubmitVoteCount = 0;
+		notificationService.notifyForGameSessionSubmissionVoteInitiated(game.getGameId(), player);
+	}
+
+	public void castGameSubmitVote(String gameId, VoteRecord voteRecord) {
+
+		PlayerSession playerSession = playerSessionMap.get(voteRecord.getVoter());
+		if (playerSession.getVoteStatus() == VoteStatus.WAITING) {
+			playerSession.setVoteStatus(voteRecord.getVoteStatus());
+			notificationService.notifyForGameSessionSubmissionVoteCasted(gameId, voteRecord);
+
+			if (voteRecord.getVoteStatus() == VoteStatus.ACCEPTED) {
+				++gameSubmitVoteCount;
+
+				if (game.getStatus() != SudokuGameStatus.FINISHED) {
+					if (isGameOverViaVoting()) {
+						stopGame();
+						notificationService.notifyForGameSessionMessageUpdate(game.getGameId(),
+								"Game Over via voting.");
+					}
+				}
+			}
+		}
+	}
+
+	public boolean isGameOverViaVoting() {
+
+		int gameOverThreshold = (int) ((1.0 / 2.0) * playerSessionMap.size());
+
+		if (gameSubmitVoteCount > gameOverThreshold) {
+			return true;
+		}
+
+		return false;
 	}
 
 }
