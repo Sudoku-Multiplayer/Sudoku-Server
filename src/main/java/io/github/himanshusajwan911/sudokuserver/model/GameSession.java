b1c08c731dcdde5e9422710392a9e212d8e7b0e4
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-15 21:21:14 +0530

    Refactor: remove gameSessionRunner, gameThread and notificationService

--- src/main/java/io/github/himanshusajwan911/sudokuserver/model/GameSession.java
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/model/GameSession.java
@@ -5,31 +5,17 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import io.github.himanshusajwan911.sudokuserver.async.GameSessionRunner;
-import io.github.himanshusajwan911.sudokuserver.exception.GameFinishedException;
-import io.github.himanshusajwan911.sudokuserver.exception.GameNotStartedException;
-import io.github.himanshusajwan911.sudokuserver.model.SudokuGame.SudokuGameStatus;
-import io.github.himanshusajwan911.sudokuserver.service.NotificationService;
-
 public class GameSession {
 
 	private SudokuGame game;
-
-	private GameSessionRunner gameSessionRunner;
-	private Thread gameThread;
-
-	private NotificationService notificationService;
 
 	private List<GameChatMessage> gameChatMessages;
 	private List<BoardUpdate> boardUpdates;
 
 	private Map<Player, PlayerSession> playerSessionMap;
 
-	public GameSession(SudokuGame game, NotificationService notificationService) {
+	public GameSession(SudokuGame game) {
 		this.game = game;
-		this.notificationService = notificationService;
-		gameSessionRunner = new GameSessionRunner(game, notificationService);
-		gameThread = new Thread(gameSessionRunner);
 		this.gameChatMessages = new ArrayList<>();
 		this.boardUpdates = new ArrayList<>();
 		this.playerSessionMap = new LinkedHashMap<>();
@@ -37,58 +23,6 @@ public class GameSession {
 
 	public SudokuGame getGame() {
 		return game;
-	}
-
-	public GameSessionRunner getGameSessionRunner() {
-		return gameSessionRunner;
-	}
-
-	public Thread getGameThread() {
-		return gameThread;
-	}
-
-	public void startGame() {
-		if (game.getStatus() == SudokuGameStatus.FINISHED) {
-			throw new GameFinishedException("Game is already finished.");
-		} else {
-			if (game.getStatus() == SudokuGameStatus.NEW) {
-				game.setStatus(SudokuGameStatus.RUNNING);
-				notificationService.notifyForGameSessionStatusUpdate(game.getGameId(), game.getStatus());
-				gameThread.start();
-			}
-		}
-	}
-
-	public void stopGame() {
-		gameSessionRunner.stopGame();
-	}
-
-	public void pauseGame() {
-		if (game.getStatus() == SudokuGameStatus.NEW) {
-			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(),
-					"Cannot pause, Game is not started yet.");
-		} else if (game.getStatus() == SudokuGameStatus.FINISHED) {
-			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(), "Cannot pause, Game is Finished.");
-		} else {
-			gameSessionRunner.pauseGame();
-		}
-	}
-
-	public void resumeGame() {
-		if (game.getStatus() == SudokuGameStatus.NEW) {
-			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(),
-					"Cannot resume, Game is not started yet.");
-		}
-
-		else if (game.getStatus() == SudokuGameStatus.FINISHED) {
-			notificationService.notifyForGameSessionMessageUpdate(game.getGameId(), "Cannot resume, Game is Finished.");
-		}
-
-		else {
-			if (game.getStatus() == SudokuGameStatus.PAUSED) {
-				gameSessionRunner.resumeGame();
-			}
-		}
 	}
 
 	public void addPlayer(Player player) {
