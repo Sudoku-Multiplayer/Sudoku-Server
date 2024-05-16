52e7e1557730e0db0c350d84b989af07a3a95c8b
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-16 20:18:04 +0530

    Feat: Add method to update board in game session
    - Implement a method to update the board in the game session identified
    by "gameSessionId".
    - Handle different game session statuses appropriately, notifying users
    via notification service.
    - Throw NoSuchGameExistsException if no game session exists with the
    provided game session id.

--- src/main/java/io/github/himanshusajwan911/sudokuserver/service/GameSessionService.java
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/service/GameSessionService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import io.github.himanshusajwan911.sudokuserver.exception.NoSuchGameExistsException;
 import io.github.himanshusajwan911.sudokuserver.model.BoardUpdate;
 import io.github.himanshusajwan911.sudokuserver.model.GameChatMessage;
 import io.github.himanshusajwan911.sudokuserver.model.GameSession;
+import io.github.himanshusajwan911.sudokuserver.model.GameSessionStatus;
 import io.github.himanshusajwan911.sudokuserver.model.SudokuGame;
 import io.github.himanshusajwan911.sudokuserver.repository.GameSessionRepository;
 
@@ -54,6 +56,36 @@ public class GameSessionService {
 	public void resumeGame(String gameSessionId) {
 		GameSessionManager gameSessionManager = gameSessionRepository.getGameSessionManager(gameSessionId);
 		gameSessionManager.resumeGame();
+	}
+
+	public void updateBoard(String gameSessionId, BoardUpdate boardUpdate) {
+
+		GameSession gameSession = gameSessionRepository.getGameSession(gameSessionId);
+
+		if (gameSession == null) {
+			throw new NoSuchGameExistsException("Game with id: " + gameSessionId + " does not exists.");
+		}
+
+		if (gameSession.getGameSessionStatus() == GameSessionStatus.RUNNING) {
+			gameSession.updateBoard(boardUpdate);
+			gameSession.addBoardUpdate(boardUpdate);
+			notificationService.notifyForGameSessionBoardUpdate(gameSessionId, boardUpdate);
+		}
+
+		else if (gameSession.getGameSessionStatus() == GameSessionStatus.NEW) {
+			notificationService.notifyForGameSessionMessageUpdate(gameSessionId,
+					"Cannot updated board, Game is not started yet.");
+		}
+
+		else if (gameSession.getGameSessionStatus() == GameSessionStatus.PAUSED) {
+			notificationService.notifyForGameSessionMessageUpdate(gameSessionId,
+					"Cannot updated board, Game is Paused.");
+		}
+
+		else if (gameSession.getGameSessionStatus() == GameSessionStatus.FINISHED) {
+			notificationService.notifyForGameSessionMessageUpdate(gameSessionId,
+					"Cannot updated board, Game is Finished.");
+		}
 	}
 
 	public void addBoardUpdate(String gameSessionId, BoardUpdate boardUpdate) {
