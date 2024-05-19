94e187c3246dc087ee3c76e6947a5da30d2d11ca
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-19 23:05:21 +0530

    feat: Enhance GameSessionRepository with GameSessionManager support
    - Introduce gameSessionManagerMap to store and manage GameSessionManager
    instances
    - Add methods to add, retrieve, and remove GameSessionManager instances
    - Maintain backward compatibility with existing GameSession methods

--- src/main/java/io/github/himanshusajwan911/sudokuserver/repository/GameSessionRepository.java
+++ src/main/java/io/github/himanshusajwan911/sudokuserver/repository/GameSessionRepository.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import io.github.himanshusajwan911.sudokuserver.model.GameSession;
+import io.github.himanshusajwan911.sudokuserver.service.GameSessionManager;
 
 @Repository
 public class GameSessionRepository {
 
 	private Map<String, GameSession> gameSessionMap = new HashMap<>();
+
+	private Map<String, GameSessionManager> gameSessionManagerMap = new HashMap<>();
 
 	public void addGameSession(String gameId, GameSession gameSession) {
 		gameSessionMap.put(gameId, gameSession);
@@ -28,6 +31,22 @@ public class GameSessionRepository {
 
 	public List<GameSession> getGameSessions() {
 		return new ArrayList<GameSession>(gameSessionMap.values());
+	}
+
+	public void addGameSessionManager(String gameSessionId, GameSessionManager gameSessionManager) {
+		gameSessionManagerMap.put(gameSessionId, gameSessionManager);
+	}
+
+	public GameSessionManager getGameSessionManager(String gameSessionId) {
+		return gameSessionManagerMap.get(gameSessionId);
+	}
+
+	public void removeGamesessionManager(String gameSessionId) {
+		gameSessionManagerMap.remove(gameSessionId);
+	}
+
+	public List<GameSessionManager> getGameSessionManagers() {
+		return new ArrayList<GameSessionManager>(gameSessionManagerMap.values());
 	}
 
 }
